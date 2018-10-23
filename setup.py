d31d22d7e50ef00abf355cf069bd8cd1a6f40615
aleccrowell <alexader.m.crowell@gmail.com>
2018-10-23 11:14:09 -0400

    fixing missing self

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='PIRS',
-      version='0.0.2',
+      version='0.0.4',
       packages=['PIRS'],
       py_modules=['rank'],
       description='Prediction Interval Ranking Score',
       url='https://github.com/aleccrowell/PIRS',
-      download_url='https://github.com/aleccrowell/PIRS/releases/tag/v0.0.2',
+      download_url='https://github.com/aleccrowell/PIRS/releases/tag/v0.0.4',
       author='Alec Crowell',
       author_email='alexander.m.crowell@gmail.com',
       license='BSD-3',
